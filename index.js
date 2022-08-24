306d2a5f086cf0010f2a7f549233b5528b6ce2ae
SirJimKe <jimmymutuku1@gmail.com>
2022-08-24 07:42:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+const p = document.createElement('p');
+
+function submitData(name, email){
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    })
+  })
+  .then(resp => resp.json())
+  .then(data => {
+    console.log(data);
+    p.textContent = data.id;
+    document.body.appendChild(p);
+  })
+  .catch(error => {
+    alert("ERROR");
+    p.textContent = error.message;
+    document.body.appendChild(p);
+  })
+}
+
+submitData("Steve", "steve@steve.com")
